746b23bbb036abeb64a7a0b0a1c5d641caf636e1
Yash Rastogi <yashrastogi@gmail.com>
2026-02-14 18:27:00 -0500

    feat(auth): implement signin signup and jwt auth (ref #1) (#2)

---
+++ database/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "./sentinent.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	);`
+
+	_, err = DB.Exec(createTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
