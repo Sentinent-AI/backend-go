6dff1f804897afa196aa70007857304d084ad4d9
= <siddharthnahar819@gmail.com>
2026-04-29 12:08:13 -0400

    style: apply gofmt to entire project

--- handlers/invitations.go
+++ handlers/invitations.go
@@ -437,7 +437,6 @@ func CancelInvitation(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
-
 func ResendInvitation(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
